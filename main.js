1495ba4c91cd12185d3fdae7685dcaccfc48ea47
Jisu <jisuc@credfin.io>
2020-04-25 16:17:12 +1000

    computed properties

---
+++ main.js
@@ -0,0 +1,55 @@
+//Add an array of sizes to the data and use v-for to display them in a list. 
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      product: 'Socks',
+      brand: 'Vue Mastery',
+      selectedVariant: 0,
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: 'green',
+          variantImage: './assets/vmSocks-green.jpg',
+          variantQuantity: 10
+        },
+        {
+          variantId: 2235,
+          variantColor: 'blue',
+          variantImage: './assets/vmSocks-blue.jpg',
+          variantQuantity: 0
+        }
+      ],
+      cart: 0,
+      onSale: true
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(index) {
+            this.selectedVariant = index
+        },
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.onSale) {
+              return this.brand + ' ' + this.product + ' are on sale!'
+            } 
+
+            return  this.brand + ' ' + this.product + ' are not on sale'
+        }
+    }
+  })
+  
+  
